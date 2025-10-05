7a19c054154f8994880c35680f70825c9dcf92af
YaoXuanZhi <yaoxuanzhi@outlook.com>
2025-10-05 17:27:39 +0800

    添加日志染色

--- UnityDemo/Assets/Scripts/Core/MiniEtLib/ET_Core/Log/NLogger.cs
+++ UnityDemo/Assets/Scripts/Core/MiniEtLib/ET_Core/Log/NLogger.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using NLog;
+using NLog.Conditions;
+using NLog.Config;
+using NLog.Targets;
 
 namespace ET
 {
@@ -12,6 +15,43 @@ namespace ET
             LogManager.Configuration = new NLog.Config.XmlLoggingConfiguration(configPath);
             LogManager.Configuration.Variables["appIdFormat"] = $"{process:000000}";
             LogManager.Configuration.Variables["currentDir"] = Environment.CurrentDirectory;
+
+            // Add colored console target
+            var consoleTarget = new ColoredConsoleTarget("coloredConsole")
+            {
+                Layout = "${longdate} [${level:uppercase=true}] ${message}",
+                UseDefaultRowHighlightingRules = false
+            };
+
+            consoleTarget.RowHighlightingRules.Add(new ConsoleRowHighlightingRule(
+                ConditionParser.ParseExpression("level == LogLevel.Trace"),
+                ConsoleOutputColor.DarkGray,
+                ConsoleOutputColor.NoChange));
+            consoleTarget.RowHighlightingRules.Add(new ConsoleRowHighlightingRule(
+                ConditionParser.ParseExpression("level == LogLevel.Debug"),
+                ConsoleOutputColor.Gray,
+                ConsoleOutputColor.NoChange));
+            consoleTarget.RowHighlightingRules.Add(new ConsoleRowHighlightingRule(
+                ConditionParser.ParseExpression("level == LogLevel.Info"),
+                ConsoleOutputColor.White,
+                ConsoleOutputColor.NoChange));
+            consoleTarget.RowHighlightingRules.Add(new ConsoleRowHighlightingRule(
+                ConditionParser.ParseExpression("level == LogLevel.Warn"),
+                ConsoleOutputColor.Yellow,
+                ConsoleOutputColor.NoChange));
+            consoleTarget.RowHighlightingRules.Add(new ConsoleRowHighlightingRule(
+                ConditionParser.ParseExpression("level == LogLevel.Error"),
+                ConsoleOutputColor.Red,
+                ConsoleOutputColor.NoChange));
+            consoleTarget.RowHighlightingRules.Add(new ConsoleRowHighlightingRule(
+                ConditionParser.ParseExpression("level == LogLevel.Fatal"),
+                ConsoleOutputColor.White,
+                ConsoleOutputColor.DarkRed));
+
+            LogManager.Configuration.AddTarget(consoleTarget);
+            LogManager.Configuration.AddRule(LogLevel.Trace, LogLevel.Fatal, consoleTarget);
+            LogManager.ReconfigExistingLoggers();
+
             this.logger = LogManager.GetLogger(name);
         }
 
